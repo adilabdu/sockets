5f38c13f31b5fea19e5753c2a7b21737cb8af7e5
Adil Abdu Bushra <adilabdu68@gmail.com>
2021-11-24 16:29:52 +0900

    New `flip` data read from `socket` event and `emit` to ESP client

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 const io = require("socket.io")(3000, {
     cors: {
-        origin: ["http://localhost:5000"],
+        origin: ["http://localhost:5000", "http://localhost:58160"],
     },
 })
 
-io.on("connection", socket => {
+let flip
+
+io.on("connection", (socket) => {
     console.log(socket.id)
+
+    socket.on('switch_state', (arg) => {
+        flip = arg
+        console.log(`New Flip State: ${ flip }`)
+        io.emit('switch_bulb', flip)
+    })
+
 })

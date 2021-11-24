973a59ff1cdbfbd4d5190bbd35016425a84be6b9
Adil Abdu Bushra <adilabdu68@gmail.com>
2021-11-24 14:18:14 +0900

    Bulb reactivity implemented with `switchFlip`

--- esp_client/src/App.svelte
+++ esp_client/src/App.svelte
@@ -1,17 +1,32 @@
 <script>
 
+  let flip = false
+
+  function switchFlip() {
+    flip = !flip
+    console.log(flip)
+  }
+
 </script>
 
 <main>
 
     <div class="wrapper">
         
-		<img id="bulb-on" src="images/bulb-on.png" alt="Lights On">
+		  <img id="bulb-on" class={ flip ? 'turn-on' : 'turn-off' } src="images/bulb-on.png" alt="Lights On">
 
     </div>
 
 </main>
 
 <style>
+
+  .turn-on {
+    visibility: visible;
+  }
+
+  .turn-off {
+    visibility: hidden;
+  }
 
 </style>

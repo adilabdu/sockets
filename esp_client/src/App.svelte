1f0c7999e09427f6b48e4554d3f807269a788bce
Adil Abdu Bushra <adilabdu68@gmail.com>
2021-11-24 14:05:29 +0900

    New mock ESP client implementation using Svelte

---
+++ esp_client/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<main>
+
+    <div class="wrapper">
+        
+		<img id="bulb-on" src="images/bulb-on.png" alt="Lights On">
+
+    </div>
+
+</main>
+
+<style>
+
+</style>
